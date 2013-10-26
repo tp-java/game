484bd8056292b7fe3901b2af34c7a9c954d4915a
root <root@maxim-HP-Pavilion-g6-Notebook-PC.(none)>
2013-10-26 09:55:35 +0400

    dk

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -10,9 +10,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Address {
 	static private AtomicInteger abonentIdCreator = new AtomicInteger();
 	final private int abonentId;
-	public int getHashCode(){
-		return abonentId;
-	}
+
 	public Address(){
 		this.abonentId = abonentIdCreator.incrementAndGet();
 	}
